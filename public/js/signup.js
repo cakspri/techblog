86f86fd5175e4de515a5f7066f2e038129de24b9
KASHYAP SHARMA <88404054+cakspri@users.noreply.github.com>
2021-09-28 13:33:13 -0700

    Add files via upload

---
+++ public/js/signup.js
@@ -0,0 +1,35 @@
+//attempt to sign the user up
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector(".username-input").value.trim();
+    const email = document.querySelector(".email-input").value.trim();
+    const password = document.querySelector(".password-input").value.trim();
+    //new accounts by default are not admins
+    const is_admin = false;
+
+    if (username && email && password) {
+        const response = await fetch("/api/user", {
+            method: "POST",
+            body: JSON.stringify({ username, email, password, is_admin }),
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.ok) {
+            document.location.replace("/");
+        } else {
+            alert(
+                "Failed to sign up. " +
+                    response.status +
+                    ": " +
+                    response.statusText
+            );
+        }
+    } else {
+        alert("Please fill out all fields.");
+    }
+};
+
+//add event listeners
+document
+    .querySelector(".signup-button")
+    .addEventListener("click", signupFormHandler);
